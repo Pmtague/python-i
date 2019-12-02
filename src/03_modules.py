f8eb0d9f64d960274753ffaa7302f57f24d3e6fb
Penny Tague <pmtague9@gmail.com>
2019-12-02 14:59:29 -0800

    04 Complete

--- src/03_modules.py
+++ src/03_modules.py
@@ -9,7 +9,7 @@ import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-print(sys.argv)
+print('Arguments:', str(sys.argv))
 
 # Print out the OS platform you're using:
 print(sys.getwindowsversion().platform)
